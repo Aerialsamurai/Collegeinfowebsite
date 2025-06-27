38e052526c89336d2347e71195f4c2377395fffa
Aerialsamurai <76955097+Aerialsamurai@users.noreply.github.com>
2025-06-27 15:23:27 +0530

    Update navbar2.jsx

--- jPEnmtRbDM26fCk1YgWSEe/edusity/src/Components/navbar2/navbar2.jsx
+++ jPEnmtRbDM26fCk1YgWSEe/edusity/src/Components/navbar2/navbar2.jsx
@@ -1,38 +1,32 @@
-// import React from "react";
-// import "./navbar2.css";
-
-// const Navbar2 = () => {
-//   const navItems = ["Location", "Ranking", "Admission", "Placement", "Fees", "Reviews"];
-
-//   return (
-//     <nav className="custom-navbar">
-//       {navItems.map((item, index) => (
-//         <React.Fragment key={item}>
-//           <div className="nav-item">
-//             {item}
-//           </div>
-//           {index !== navItems.length - 1 && <div className="divider" />}
-//         </React.Fragment>
-//       ))}
-//     </nav>
-//   );
-// };
-
-// export default Navbar2;
-// Navbar2.jsx
-
 import React from "react";
 import "./navbar2.css";
+import { Link } from "react-scroll"; // only if you're using scroll behavior
 
 const Navbar2 = () => {
-  const navItems = ["Overview","Location", "Ranking", "Admission", "Placement", "Fees", "Reviews"];
+  const navItems = [
+    { name: "Overview", target: "overview" },
+    { name: "Location", target: "location" },
+    { name: "Ranking", target: "ranking" },
+    { name: "Admission", target: "admission" },
+    { name: "Placement", target: "placement" },
+    { name: "Fees", target: "fees" },
+    { name: "Reviews", target: "reviews" }
+  ];
 
   return (
     <nav className="custom-navbar">
-      {navItems.map((item, index) => (
-        <div key={item} className="nav-item-wrapper">
-          <div className="nav-item">{item}</div>
-          {/* {index !== navItems.length - 1 && <div className="divider" />} */}
+      {navItems.map((item) => (
+        <div key={item.name} className="nav-item-wrapper">
+          {/* Optional scroll behavior with Link */}
+          <Link
+            to={item.target}
+            smooth={true}
+            offset={-100}
+            duration={500}
+            className="nav-item"
+          >
+            {item.name}
+          </Link>
         </div>
       ))}
     </nav>
