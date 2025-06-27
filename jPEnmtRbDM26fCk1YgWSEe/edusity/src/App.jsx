ddda59b56b01d1d24130127055f9b552dc9c8bb5
Brazen37 <148343604+Brazen37@users.noreply.github.com>
2025-06-27 16:24:28 +0530

    Update App.jsx

--- jPEnmtRbDM26fCk1YgWSEe/edusity/src/App.jsx
+++ jPEnmtRbDM26fCk1YgWSEe/edusity/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
