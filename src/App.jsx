875b5905aeb63705e130c8ba1f5f3ff5066fbe27
AkshatDubey1611 <f20230954@pilani.bits-pilani.ac.in>
2025-07-03 18:04:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,65 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Element } from "react-scroll";
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Title from './Components/Title/Title'
-import Navbar2 from './Components/navbar2/navbar2'
-import Location from './Components/Locationtemp/location'
-import Overview from './Components/Overview/overview'
-import Review from './Components/Review/review'
-import Sehom from './Components/Seathome/seathome'
-import Cuthom from'./Components/Cuthome/cuthome'
-import Footer from './Components/Footer/Footer'
-import Seatmatrix from './Components/Seatmatrix/seatmatrix'
-import Cutoff from './Components/Cutoff/cutoff'
-import Ranking from './Components/Rankings/rank'
-import Fees from './Components/Fees/fees'
-import Admission from'./Components/Admission/admission'
-import Placement from './Components/Placement/placement'
-import Mainlayout from './Components/Mainlayout/mainlayout'
+
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Title from './Components/Title/Title';
+import Navbar2 from './Components/navbar2/navbar2';
+import Location from './Components/Locationtemp/location';
+import Overview from './Components/Overview/overview';
+import Review from './Components/Review/review';
+import Sehom from './Components/Seathome/seathome';
+import Cuthom from './Components/Cuthome/cuthome';
+import Footer from './Components/Footer/Footer';
+import Seatmatrix from './Components/Seatmatrix/seatmatrix';
+import Cutoff from './Components/Cutoff/cutoff';
+import Ranking from './Components/Rankings/rank';
+import Fees from './Components/Fees/fees';
+import Admission from './Components/Admission/admission';
+import Placement from './Components/Placement/placement';
+import Mainlayout from './Components/Mainlayout/mainlayout';
+import Cutoff2024 from './Components/Cutoff2024/cutoff2024';
+import Cutoff2023 from './Components/Cutoff2023/cutoff2023';
 
 const App = () => {
-
-  
-
   return (
-      
-      <Routes>
-        {/*  Route for homepage */}
-        <Route path="/" element={
-    <div>
-     <Navbar/>
-     <Hero/>
-     <Navbar2/>
-    
-     <Mainlayout>
-     <Element name="Overview"><Overview/></Element>
+    <Routes>
+      {/* Homepage route */}
+      <Route path="/" element={
+        <div>
+          <Navbar />
+          <Hero />
+          <Navbar2 />
 
-     <Element name="Location"><Location/></Element>
-     
+          <Mainlayout>
+            <Element name="Overview"><Overview /></Element>
+            <Element name="Location"><Location /></Element>
+            <Element name="Fees"><Fees /></Element>
+            <Element name="Ranking"><Ranking /></Element>
+            <Element name="Seat Matrix"><Sehom /></Element>
+            <Element name="Cutoffs"><Cuthom /></Element>
+            <Element name="Admission"><Admission /></Element>
+            <Element name="Placement"><Placement /></Element>
+          </Mainlayout>
 
-     <Element name="Fees"><Fees/></Element>
+          <div className="container">
+            <Title subTitle='REVIEWS' title='What Student Says' />
+          </div>
+          <Element name="Reviews"><Review /></Element>
+          <Footer />
+        </div>
+      } />
 
-     <Element name="Ranking"><Ranking/></Element>
-     <Element name="Seat Matrix"><Sehom/></Element>
-     <Element name="Cutoffs"><Cuthom/></Element>
-     
+      {/* Seat Matrix page */}
+      <Route path="/Seatmatrix" element={<Seatmatrix />} />
 
-     <Element name="Admission"><Admission/></Element>
-     <Element name="Placement"><Placement/></Element>
-     </Mainlayout>
-     
-     <div className="container">
-       <Title subTitle='REVIEWS' title='What Student Says'/>
-       
-     </div>
-      <Element name="Reviews"><Review/></Element>
-        
-       
-        <Footer/>
-   
-    </div>
-    }/>
-    {/*  Route for SeatMatrix page */}
-        <Route path="/Seatmatrix" element={<Seatmatrix />} />
+      {/* Cutoff pages */}
+      <Route path="/Cutoff" element={<Cutoff />} />
+      <Route path="/cutoff2024" element={<Cutoff2024 />} />
+      <Route path="/cutoff2023" element={<Cutoff2023 />} />
+    </Routes>
+  );
+};
 
-        {/*  Route for Cutoffs page */}
-        <Route path="/Cutoff" element={<Cutoff />} />
-      </Routes>
-    
-  )
-}
-
-export default App
+export default App;
