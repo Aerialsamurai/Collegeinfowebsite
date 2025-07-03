4fb0fd0cbb3946c99cedab42aa14103429920902
AkshatDubey1611 <f20230954@pilani.bits-pilani.ac.in>
2025-07-03 18:00:19 +0530

    Update cuthome.jsx

--- src/Components/Cuthome/cuthome.jsx
+++ src/Components/Cuthome/cuthome.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 const Cuthom = () => {
   return (
     <div className="cuthom-container">
-      
-     <h3 className="cuthom-head">Cutoffs</h3>
+      <h3 className="cuthom-head">Cutoffs</h3>
       <div className="cuthom-box">
-        
-         <p>
-  <Link to="/Cutoff" className="cuthom-know-more">Cutoff 2025</Link></p>
-  <p classname="cuthom-know-more">Cutoff 2024</p>
-  <p classname="cuthom-know-more">Cutoff 2023</p>
-  <p classname="cuthom-know-more">Cutoff 2022</p>
-
+        <p>
+          <Link to="/Cutoff" className="cuthom-know-more">Cutoff 2025</Link>
+        </p>
+        <p>
+          <Link to="/cutoff2024" className="cuthom-know-more">Cutoff 2024</Link>
+        </p>
+        <p>
+          <Link to="/cutoff2023" className="cuthom-know-more">Cutoff 2023</Link>
+        </p>
       </div>
     </div>
   );
